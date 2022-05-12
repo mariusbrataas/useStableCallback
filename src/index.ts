5edfd0d263ade7bf6b5d90fb0a647e6776aac5a0
Marius Brataas <mariusbrataas@me.com>
2022-05-12 12:42:18 +0200

    Init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { useCallback, useRef } from 'react';
+
+/**
+ * Returns a memoized version of your function that maintains a stable reference, but
+ * also can read the latest scope (props and state) of the component in which it is used.
+ */
+export default function useStableCallback<Args extends any[], T>(
+  callback: (...args: Args[]) => T
+) {
+  const callbackContainer = useRef(callback);
+  callbackContainer.current = callback;
+  return useCallback(
+    (...args: Args) => callbackContainer.current(...args),
+    [callbackContainer]
+  );
+}
